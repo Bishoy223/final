e531857bababb44dd6450a73cce808f0eb054ca5
Bishoy223 <114161500+Bishoy223@users.noreply.github.com>
2024-12-21 01:38:50 +0200

    Update Transaction.java

--- src/BankManagementSystem/Transaction.java
+++ src/BankManagementSystem/Transaction.java
@@ -1,64 +1,37 @@
 package BankManagementSystem;
 
-import java.time.LocalDateTime;
-
-class Transaction {
-    private String transactionId;
-    private String accountId;
+public class Transaction {
+    private String date;
+    private int clientId;
+    private int employeeId;
     private double amount;
-    private LocalDateTime timestamp;
-    private String description;
-    private TransactionType type;
 
-    public enum TransactionType {
-        DEPOSIT,
-        WITHDRAWAL,
-        TRANSFER_IN,
-        TRANSFER_OUT,
-        CREDIT_CARD_PAYMENT
-    }
-
-    public Transaction(String transactionId, String accountId, double amount, TransactionType type, String description) {
-        this.transactionId = transactionId;
-        this.accountId = accountId;
+    public Transaction(String date, int clientId, int employeeId, double amount) {
+        this.date = date;
+        this.clientId = clientId;
+        this.employeeId = employeeId;
         this.amount = amount;
-        this.timestamp = LocalDateTime.now();
-        this.type = type;
-        this.description = description;
     }
 
-    public String getTransactionId() {
-        return transactionId;
+    public String getDate() {
+        return date;
     }
 
-    public String getAccountId() {
-        return accountId;
+    public int getClientId() {
+        return clientId;
     }
 
-    public double getAmount() {
-        return amount;
-    }
-
-    public LocalDateTime getTimestamp() {
-        return timestamp;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public TransactionType getType() {
-        return type;
+    public int getEmployeeId() {
+        return employeeId;
     }
 
     @Override
     public String toString() {
-        return "Transaction[ID=" + transactionId +
-                ", AccountID=" + accountId +
-                ", Amount=" + amount +
-                ", Type=" + type +
-                ", Description=" + description +
-                ", Timestamp=" + timestamp + "]";
+        return "Transaction{" +
+                "date='" + date + '\'' +
+                ", clientId=" + clientId +
+                ", employeeId=" + employeeId +
+                ", amount=" + amount +
+                '}';
     }
 }
-
